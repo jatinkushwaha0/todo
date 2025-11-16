089a3199c76c2c3a06d27d1a68611a290ff973f7
JATIN KUSHWAHA <jatinkushwaha@gmail.com>
2025-11-16 23:15:34 +0530

    To-Do List with HTML,CSS,JS

---
+++ script.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('input');
+const sub = document.querySelector('.submit');
+const del =document.querySelector('.delete');
+const li = document.querySelector('.li');
+const ol = document.querySelector('.ol');
+sub.addEventListener('click', function () {
+    if(input.value.trim()  === ""){ }
+    else {
+  let value = input.value;
+    let insert = `<li class="li">${value}</li>`
+    ol.innerHTML+=(insert);
+    input.value="";
+    }
+
+});
